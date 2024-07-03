8ece1c35b6b06b9257b8cebd69f496e8aa926399
Zirui Wang <wangzr020409@gmail.com>
2024-07-03 00:51:12 -0400

    Update setup.py
    change name of package

--- lib/pointgroup_ops/setup.py
+++ lib/pointgroup_ops/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='PG_OP',
+    name='pointgroup_ops',
     ext_modules=[
         CUDAExtension('PG_OP', [
             'src/pointgroup_ops_api.cpp',
